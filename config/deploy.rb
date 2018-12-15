3d0c3d3612c93f9994fc2100a48499bef962911a
sghrm <sugaharamasaki0530@gmail.com>
2018-12-15 20:53:42 +0900

    Syntax error-modify

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
 # secrets.yml用のシンボリックリンクを追加
-set :linked_files, %
+set :linked_files, %w{ config/secrets.yml }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
